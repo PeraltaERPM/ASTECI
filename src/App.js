8d1cb7b8a92ad657655e8e5a3dc5d663b1010f56
Erick Peralta <erick.peralta@ltte.com.mx>
2022-09-23 13:20:11 -0500

    Se agrega proyecto

--- src/App.js
+++ src/App.js
@@ -1,24 +1,70 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import 'styled-components';
+import DataTable from 'react-data-table-component';
+import './style.scss';
 
-function App() {
+const App = () => {
+
+  //configuracon de hooks
+  const [result, setResult] = useState( [] )
+
+  //Funcion para mostrar datos
+
+  const URL = 'https://api2.binance.com/api/v3/ticker/24hr'
+  const showData = async () => {
+    const response = await fetch(URL) 
+    const data     = await response.json()
+    console.log(data)
+    setResult(data)
+  }
+
+  useEffect ( () => {
+    showData()
+  }, [])
+  //Configuracion de las columnas
+
+  const columns = [
+    {
+      name: 'Symbol',
+      selector: row => row.symbol
+    },
+    {
+      name: 'priceChange',
+      selector: row => row.priceChange
+    },
+    {
+      name: 'price Change Percent',
+      selector: row => row.priceChangePercent
+    },
+    {
+      name: 'openPrice',
+      selector: row => row.openPrice
+    },
+    {
+      name: 'high Price',
+      selector: row => row.highPrice
+    },
+    {
+      name: 'low Price',
+      selector: row => row.lowPrice
+    },
+    
+  ]  
+
+  //Mostrar los datos en la tabla
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className='App'>
+    <div className='row'>
+    <DataTable
+    title="Prueba ASTECI" 
+    columns={columns}
+    data={result}
+    striped
+    pagination
+    />
     </div>
+   </div>
   );
 }
 
